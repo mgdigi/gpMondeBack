64085209154ae1c30d169729d736d40f5286adfb
mgdigi <gueyeprophete287@gmail.com>
2025-08-12 22:00:57 +0000

    gerer le cors

---
+++ server.js
@@ -0,0 +1,14 @@
+import jsonServer from 'json-server';
+import cors from 'cors';
+
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+server.use(cors());
+server.use(middlewares);
+server.use(router);
+
+server.listen(3001, () => {
+  console.log('JSON Server is running on port 3001');
+});
